02eaf8682943c557ee952f1d5879fe67dd01dd75
sasasak <bokyong97@naver.com>
2026-07-31 09:59:14 +0900

    feat: 검색 페이지 상태 관리, 컴포넌트 조립
    - SearchClient : page.tsx의 경우 서버 컴포넌트로 상태 관리가 불가능
    - SearchClient가 클라이언트 컴포넌트로서 상태 관리, 컴포넌트 조립의 역할을 대신함

---
+++ src/app/(main)/search/SearchClient.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { useState, useRef } from 'react';
+import RecentSearches from './_components/RecentSearches';
+import SearchBar from './_components/SearchBar';
+
+interface SearchClientProps {
+  userId: string;
+}
+
+export default function SearchClient({ userId }: SearchClientProps) {
+  const [query, setQuery] = useState('');
+  const refetchRef = useRef<(() => void) | null>(null);
+
+  const handleSearch = async () => {
+    await new Promise((resolve) => setTimeout(resolve, 300));
+    refetchRef.current?.();
+  };
+
+  return (
+    <div className="flex flex-col gap-6">
+      <SearchBar
+        query={query}
+        setQuery={setQuery}
+        userId={userId}
+        onSearch={handleSearch}
+      />
+      <RecentSearches
+        userId={userId}
+        query={query}
+        setQuery={setQuery}
+        refetchRef={refetchRef}
+      />
+      {query && <div>{/* 나중에 검색 결과 컴포넌트 */}</div>}
+    </div>
+  );
+}
